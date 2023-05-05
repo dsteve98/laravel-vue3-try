0916397681b9c085036f0246ab5df4d89a5af9a3
zaayn <muzayyin937@gmail.com>
2023-05-05 13:14:21 +0700

    revert

--- resources/js/components/ToDoComponent.vue
+++ resources/js/components/ToDoComponent.vue
@@ -10,29 +10,25 @@ import {mapGetters} from "vuex";
 export default {
   data: function () {
     return {
-      // list: [
-      //   { title: "menyapu", status: false },
-      //   { title: "cuci piring", status: false },
-      //   { title: "pel", status: false },
-      // ],
+      list: [
+        { title: "menyapu", status: false },
+        { title: "cuci piring", status: false },
+        { title: "pel", status: false },
+      ],
     };
   },
   computed: {
-    ...mapGetters({
-      list: 'getToDO'
-    })
+    
   },
   methods: {
     addList(datainput) {
       let newList = {
         title: datainput,
       };
-      // this.list.push(newList);
-      this.$store.dispatch('addList', newList)
+      this.list.push(newList);
     },
     deleteList(index) {
-      // this.list.splice(index, 1);
-      this.$store.dispatch('deleteList', index)
+      this.list.splice(index, 1);
     },
   },
   mounted() {
