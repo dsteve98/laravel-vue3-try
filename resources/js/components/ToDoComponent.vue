993c0fc1b9e32885ae925f42c5a1a889e1bb9921
zaayn <muzayyin937@gmail.com>
2023-05-05 13:48:01 +0700

    add todo

--- resources/js/components/ToDoComponent.vue
+++ resources/js/components/ToDoComponent.vue
@@ -6,30 +6,27 @@
   </div>
 </template>
 <script>
+
 import {mapGetters} from "vuex";
 export default {
   data: function () {
     return {
-      list: [
-        { title: "menyapu", status: false },
-        { title: "cuci piring", status: false },
-        { title: "pel", status: false },
-      ],
+      
     };
   },
   computed: {
-    
+    ...mapGetters({
+      list: 'getToDo'
+    })
   },
   methods: {
     addList(datainput) {
       let newList = {
         title: datainput,
       };
-      this.list.push(newList);
-    },
-    deleteList(index) {
-      this.list.splice(index, 1);
-    },
+      // this.list.push(newList);
+      this.$store.dispatch('addList', newList)
+    }
   },
   mounted() {
     console.log("Component mounted.");
--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -2,15 +2,25 @@ import {createStore} from 'vuex'
 
 export default createStore ({
     state: {
-        
+        list: [
+            { title: "menyapu", status: false },
+            { title: "cuci piring", status: false },
+            { title: "pel", status: false },
+        ],
     },
     getters: {
-        
+        getToDo(state){
+            return state.list
+        }
     },
     actions: {
-        
+        addList(context, payload){
+            context.commit('ADD_TODO',payload)
+        }
     },
     mutations: {
-        
+        ADD_TODO(state, payload){
+            state.list.push(payload)
+        }
     }
 })
